121007ed518f24f72ed4585264a499603c3531c5
Rainfire267 <ejimenez267@gmail.com>
2021-04-22 15:54:18 +0000

    actualizado el body

--- src/front/js/pages/sectionTab.js
+++ src/front/js/pages/sectionTab.js
@@ -12,7 +12,7 @@ const SectionTab = () => {
 	return (
 		<Tab.Container id="left-tabs-example" defaultActiveKey="first">
 			<Row>
-				<Col md={4} className="offset-4">
+				<Col md={4} className="offset-4" style={{textAlign: "center"}}>
 					<Nav variant="pills" className="flex-row">
 						<Nav.Item>
 							<Nav.Link eventKey="first">Dueño</Nav.Link>
