b2507c556c6c9bd770db9b16c1ad8668acbea157
PitiTheo <julio.vilaboa@yandex.com>
2021-04-24 04:01:02 +0000

    modeling casi completo

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,72 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from eralchemy import render_er
 
-db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+Base = declarative_base()
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(30),unique=True, nullable=False)
+    last_name = Column(String(30),unique=False,nullable=False)
+    birth_date = Column(Integer,unique=True, nullable=False)
+    email = Column(String(30),unique=True, nullable=False)
+    password = Column(String(30))
+    id_address = Column(String(100), unique = False, nullable = False)
+    phone = Column(Integer,unique=True, nullable=False)
+    user_rol = Column(Integer, unique = False, nullable = False)
+    is_active = Column(Boolean(), unique=False, nullable=False)
+    
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+
+
+class Owner(Base):
+    __tablename__ = 'owner'
+    id = Column(Integer, primary_key=True)
+    pet_id = Column(Integer)
+    pet = relationship("Pet", uselist=False, back_populates="owner")
+    
+
+class Buddy(Base):
+    __tablename__ = 'buddy'
+    id = Column(Integer, primary_key=True)
+    service = Column(String(30))
+    user_id = Column(ForeignKey ('user.id'))
+    user_id = Column(Integer, ForeignKey ('comment.id'))
+    
+   
+    
+class Address(Base):
+    __tablename__ = 'address'
+    id = Column(Integer, primary_key=True)
+    id_comment = Column(String(200))
+    user_id = Column(Integer, ForeignKey ('user.id'))
+    
+    
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_body= Column(String(200))
+    user_id = Column(Integer, ForeignKey ('user.id'))
+
+class Pet(Base):
+    __tablename__ = 'pet'
+    id = Column(Integer, primary_key=True)
+    pet_id = Column(Integer)
+    pet_name = Column(String(30),unique=True, nullable=False)
+    owner_id = Column(Integer, ForeignKey('owner.id'))
+    owner = relationship("Owner", back_populates="pet")
+    
+
+
+    def to_dict(self):
+        return {}
+
+## Draw from SQLAlchemy base
+render_er(Base, 'diagram.png')
