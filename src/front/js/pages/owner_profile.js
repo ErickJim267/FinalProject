7c0a8f29d69b3e9d99f0e44a414bf47f2cd53236
Rainfire267 <ejimenez267@gmail.com>
2021-04-25 18:00:36 +0000

    actualizado perfil del pet

--- src/front/js/pages/owner_profile.js
+++ src/front/js/pages/owner_profile.js
@@ -5,7 +5,7 @@ import "../../styles/owner_profile.scss";
 export const Owner_profile = () => {
 	return (
 		<>
-			<div>
+			<div className="profileCont">
 				<Row>
 					<Col md={9}>
 						<>
@@ -32,9 +32,9 @@ export const Owner_profile = () => {
 							/>
 							<Card.Body>
 								<Card.Text>
-									<div>Some info here</div>
-									<div>Some info here</div>
-									<div>Some info here</div>
+									<div>nombre del dueño</div>
+									<div>info</div>
+									<div>info</div>
 								</Card.Text>
 								{/*<Button variant="primary">Añade una reservación</Button>*/}
 							</Card.Body>
@@ -56,12 +56,12 @@ export const Owner_profile = () => {
 										<Card.Title>Cuidados especiales</Card.Title>
 										<Card.Text>
 											<ul>
-												<li>Que no vea al padre porque se pone molesto</li>
-												<li />
-												<li />
-												<li />
-												<li />
-												<li />
+												<li>Sin fotos</li>
+												<li>No intentar hacerlo reir</li>
+												<li>No decirle lindo</li>
+												<li>Text here</li>
+												<li>Text here</li>
+												<li>Text here</li>
 											</ul>
 										</Card.Text>
 									</Card.Body>
@@ -76,12 +76,12 @@ export const Owner_profile = () => {
 										<Card.Title>Mi personalidad</Card.Title>
 										<Card.Text>
 											<ul>
-												<li />
-												<li />
-												<li />
-												<li />
-												<li />
-												<li />
+												<li>Todo me aburre</li>
+												<li>Quiero tranquilidad</li>
+												<li>no me gusta jugar</li>
+												<li>No me gusta tener vecinos</li>
+												<li>No me gusta nada</li>
+												<li>No quiero nada</li>
 											</ul>
 										</Card.Text>
 									</Card.Body>
@@ -96,12 +96,12 @@ export const Owner_profile = () => {
 										<Card.Title>Lo que debes saber de mí</Card.Title>
 										<Card.Text>
 											<ul>
-												<li />
-												<li />
-												<li />
-												<li />
-												<li />
-												<li />
+												<li>No tengo enfermedades</li>
+												<li>No tengo operaciones</li>
+												<li>No tengo alergias</li>
+												<li>No tengo chip</li>
+												<li>Solo castracion</li>
+												<li>Text here</li>
 											</ul>
 										</Card.Text>
 									</Card.Body>
