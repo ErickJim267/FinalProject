abf4683191b3a88f59b58ec413c645b33c7f438d
jcarrill0 <josecarrillo8@gmail.com>
2021-04-26 14:57:51 -0600

    Api register complete

--- src/api/routes.py
+++ src/api/routes.py
@@ -3,7 +3,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 import os
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Owner, Buddy
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
@@ -19,9 +19,9 @@ def register():
     password = request.json.get('password', None)
     email = request.json.get('email', None)
     name = request.json.get('name', None)
-    user_rol = request.json.get('rol', None)
+    user_role = request.json.get('rol', None)
     ## tipo de usuario
-    last_name = request.json.get('name', None)
+    last_name = request.json.get('last_name', None)
     
     if password is None:
         return 'You need to specify the password', 400
@@ -31,7 +31,7 @@ def register():
         return 'You need to specify the name', 400
     if last_name is None:
         return 'You need to specify the last_name', 400
-    if user_rol is None:
+    if user_role is None:
         return 'You need to specify the user_rol', 400
 
     user = User.query.filter_by(email=email).first()
@@ -39,9 +39,18 @@ def register():
     if user:
         return jsonify({"msg" : "User already exist"})
     else: 
-        new_user = User(email=email, password=password)
+        new_user = User(email=email, password=password, name=name, last_name=last_name, user_role=user_role, is_active=True)
         db.session.add(new_user)
         db.session.commit()
+        if user_role == 'owner':
+            new_owner = Owner(user_id=new_user.id)    
+            db.session.add(new_owner)
+            db.session.commit()
+        else:
+            new_buddy = Buddy(user_id=new_user.id)    
+            db.session.add(new_buddy)
+            db.session.commit()
+        
         return jsonify({"msg" : "User added successfully!"}), 200
 
 @api.route('/login', methods=['POST'])
