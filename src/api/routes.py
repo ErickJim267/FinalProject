83836beada6f200c4d3af8d5974c80b663841609
Rainfire267 <ejimenez267@gmail.com>
2021-04-27 05:15:30 +0000

    añadidos token y register

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,7 +6,6 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Owner, Buddy
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
-
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
@@ -14,9 +13,14 @@ import os
 
 api = Blueprint('api', __name__)
 
-# @api.route('/token', methods=['POST'])
-# def handle_token():
-#    return jsonify(response_body), 200
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user.id
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return User.query.filter_by(id=identity).one_or_none()
 
 @api.route('/register', methods=['POST'])
 def register():
@@ -57,8 +61,10 @@ def register():
         
         return jsonify({"msg" : "User added successfully!"}), 200
 
-@api.route('/login', methods=['POST'])
-def signin():
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/token", methods=["POST"])
+def create_token():
     password = request.json.get('password', None)
     email = request.json.get('email', None)
 
@@ -76,20 +82,9 @@ def signin():
         # create a new token with user_id
         access_token = create_access_token(identity=user, expires_delta=timedelta(hours=80))
         return jsonify({"token" : access_token, "user_id" : user.id}), 200
-# Create a route to authenticate your users and return JWTs. The
-# create_access_token() function is used to actually generate the JWT.
-@api.route("/token", methods=["POST"])
-def create_token():
-    email = request.json.get("email", None)
-    password = request.json.get("password", None)
-    if email != "test" or password != "test":
-        return jsonify({"msg": "Bad email or password"}), 401
-
-    access_token = create_access_token(identity=email)
-    return jsonify(access_token=access_token)
 
 #############################################################################
-######### Esto se creó para las rutas de los componentes     ################
+###########  Esto se creó para las rutas de los componentes  ################
 #############################################################################
 
 @api.route('/register', methods=['POST', 'GET'])
